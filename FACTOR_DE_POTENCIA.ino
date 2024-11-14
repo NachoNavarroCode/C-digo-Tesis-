6a0fdabd0e879cd6eaec67c4db11d36a8ba25a4b
NachoNavarroCode <nachonavarrox@hotmail.com>
2024-11-13 19:38:08 -0600

    Update FACTOR_DE_POTENCIA.ino

--- FACTOR_DE_POTENCIA.ino
+++ FACTOR_DE_POTENCIA.ino
@@ -11,21 +11,21 @@ void setup() {
 
 void loop() {
   
-  // Lee el valor del Voltaje
+  // Voltage reading
   sensorValueV = analogRead(sensorV);
   valorealV = ((sensorValueV / 1023.0) * 5000.0) -230;
 
-  // Leer el valor de Corriente
+  // Current reading
   sensorValueA = analogRead(sensorA);
   valorealA = ((sensorValueA / 1023.0) * 1000.0) -670;
   
-  // Calcula el ángulo de desfasamiento en radianes
+  // Phase angle calculation (radians)
   float desfR = atan2(valorealV, valorealA);
 
-  //Convierte radianes a grados
+  //Radians to Degrees convertion
   float desfG = degrees(desfR);
 
-  // Calcula el factor de potencia
+  // Power Factor calculation
   float powerFactor = abs(cos(desfR));
 
   
